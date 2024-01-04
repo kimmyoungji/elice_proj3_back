86dc34a3c5d8beb9dcd43f02130599a928f7915b
[AI9]허제인 <codingjamee@gmail.com>
2024-01-05 03:13:27 +0900

    Feat: useControlled hook을 사용한 InputCommon

--- src/components/UI/InputCommon.tsx
+++ src/components/UI/InputCommon.tsx
@@ -6,8 +6,60 @@
 // disabled: boolean;
 // names: string[];
 
-const InputCommon = () => {
-  return <div></div>;
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { useControlled } from "../../hooks/useControlled";
+
+type InputPropsType = {
+  value?: string | number | undefined;
+  defaultValue?: string | number;
+  className?: string;
+  onBlur?: any;
+  placeholder?: string;
+  disabled?: boolean;
+  required?: boolean;
+  type?: string;
+  onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void;
 };
+
+//inputCommon에 value를 넣으면 controlled. 기본은 uncontrolled
+//uncontrolled일때만 input을 자동 변경하게 해줌
+const InputCommon = forwardRef(
+  ({ value = undefined, defaultValue, ...props }: InputPropsType, ref) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [input, setInput] = useControlled({
+      value,
+      defaultValue,
+    });
+
+    //focus기능
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          focus() {
+            inputRef.current?.focus();
+          },
+        };
+      },
+      []
+    );
+
+    return (
+      <>
+        {input ? (
+          <input
+            value={input}
+            onChange={(e) => setInput(e)}
+            ref={inputRef}
+            {...props}
+          />
+        ) : (
+          <input value={input} ref={inputRef} {...props} />
+        )}
+      </>
+    );
+  }
+);
 
 export default InputCommon;
