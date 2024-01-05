acf7074e1593adaf1dff8130ac3a700f18788fbe
dev-vming <dev.vming@gmail.com>
2024-01-05 22:52:24 +0900

    Feat: AI 식단 카메라 UI 작성

--- src/components/pages/add-photo/AddPhoto.tsx
+++ src/components/pages/add-photo/AddPhoto.tsx
@@ -1,5 +1,30 @@
+import styles from "../../UI/headerCommon.module.css";
+import style from "./addphoto.module.css";
+
 const AddPhoto = () => {
-  return <div>AddPhoto</div>;
+  return (
+    <>
+      <div className={styles.header}>
+        <img className={styles.arrow} src="icons/left_arrow.png" alt="뒤로가기" />
+        <div className={styles.text}>AI 식단 카메라</div>
+      </div>
+      <div className={style.cam}>
+        촬영 화면 들어갈 공간
+      </div>
+      <div className={style.addbox}>
+        <div className={style.item}>
+          <img className={style.icon} src="icons/album.png" alt="앨범" />
+          <div className={style.text}>앨범</div>
+        </div>
+        <button className={style.shotbtn}></button>
+        <div className={style.item}>
+          <img className={style.icon} src="icons/self.png" alt="직접입력" />
+          <div className={style.text}>직접입력</div>
+        </div>
+        
+      </div>
+    </>
+  );
 };
 
 export default AddPhoto;
