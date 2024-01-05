ba6671d93389d72a8e8a8026d8558f23daba466a
[AI9]허제인 <codingjamee@gmail.com>
2024-01-05 21:57:03 +0900

    Fix: ButtonCommon에 여러 클래스 적용되는 기능

--- src/components/UI/ButtonCommon.tsx
+++ src/components/UI/ButtonCommon.tsx
@@ -1,46 +1,35 @@
-import React, {
-  ComponentPropsWithRef,
-  ReactElement,
-  useId,
-  useImperativeHandle,
-  useRef,
-} from "react";
-import classes from "./buttonCommon.module.css";
+import React, { ComponentPropsWithRef, ReactElement, useId, useImperativeHandle, useRef } from 'react';
+import classes from './buttonCommon.module.css';
 
 type ButtonPropsType = {
   children?: string | ReactElement | number;
   className?: string;
   onClickBtn?: () => void;
-} & ComponentPropsWithRef<"button">;
+} & ComponentPropsWithRef<'button'>;
 
-const ButtonCommon = React.forwardRef(
-  ({ className, children, onClickBtn }: ButtonPropsType, ref) => {
-    const buttonRef = useRef<HTMLButtonElement>(null);
-    const id = useId();
+const ButtonCommon = React.forwardRef(({ className, children, onClickBtn }: ButtonPropsType, ref) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const id = useId();
 
-    useImperativeHandle(
-      ref,
-      () => {
-        return {
-          focus() {
-            buttonRef.current?.focus();
-          },
-        };
-      },
-      []
-    );
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        focus() {
+          buttonRef.current?.focus();
+        },
+      };
+    },
+    []
+  );
 
-    return (
-      <button
-        id={id}
-        className={`${classes.button} ${className ? classes[className] : ""}`}
-        onClick={onClickBtn}
-        ref={buttonRef}
-      >
-        {children}
-      </button>
-    );
-  }
-);
+  const buttonClass = Array.isArray(className) ? className.map((classN) => classes[classN]) : className;
+
+  return (
+    <button id={id} className={`${classes.button} ${buttonClass}`} onClick={onClickBtn} ref={buttonRef}>
+      {children}
+    </button>
+  );
+});
 
 export default ButtonCommon;
