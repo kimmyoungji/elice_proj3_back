197ddd322d6b1f49542d232c2a098afa5ef4b0e2
[AI9]허제인 <codingjamee@gmail.com>
2024-01-08 19:52:00 +0900

    Feat: getDates util함수

---
+++ src/utils/getDates.ts
@@ -0,0 +1,24 @@
+const getDates = () => {
+  const date = new Date();
+  const thisYear = date.getFullYear();
+  const thisMonthNum = date.getMonth() + 1;
+  const thisMonthwithzero = thisMonthNum >= 10 ? thisMonthNum : `0${thisMonthNum}`;
+  const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+  const thisDayText = dayOfWeek[date.getDay()];
+  const thisHour = date.getHours();
+
+  const getNowMealText = () => {
+    if (thisHour >= 5 && thisHour < 11) {
+      return '아침';
+    } else if (thisHour >= 11 && thisHour < 17) {
+      return '점심';
+    } else if (thisHour >= 17 && thisHour < 23) {
+      return '저녁';
+    } else {
+      return '간식';
+    }
+  };
+
+  return { thisYear, thisMonth: thisMonthwithzero, thisDayText, getNowMealText };
+};
+export default getDates;
