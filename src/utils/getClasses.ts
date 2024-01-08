7241e55894e9346dbde2a9444228663ce3102108
[AI9]허제인 <codingjamee@gmail.com>
2024-01-08 17:08:03 +0900

    Feat: className설정 파일 따로 분리

---
+++ src/utils/getClasses.ts
@@ -0,0 +1,8 @@
+export const getClassName = (classes: any | undefined) => {
+  const classArray: string[] | undefined = classes?.split(' ');
+  return classArray
+    ? classArray.map((classN) => classes && classes[classN]).join(' ')
+    : classes
+    ? classes[classes]
+    : '';
+};
