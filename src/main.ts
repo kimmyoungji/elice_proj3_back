c04b5b04630e63a460119675fbe3d8f19a0a2671
[AI9] 최유림 <ul051101@gmail.com>
2024-01-08 18:22:05 +0900

    [유림] Feat: 기본 파일 구조 생성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { setupSwagger } from "src/util/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(5001);
 }
 bootstrap();
