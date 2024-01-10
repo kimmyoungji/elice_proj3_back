81fe3721a59731743475e5d9efb91b6f8b505f5c
dev-vming <dev.vming@gmail.com>
2024-01-10 23:25:53 +0900

    Feat: AI 식단 확인 페이지 수정 컴포넌트 UI 작성

--- src/components/pages/record/RecordEditDetail.tsx
+++ src/components/pages/record/RecordEditDetail.tsx
@@ -1,10 +1,78 @@
+import InputCommon from "@components/UI/InputCommon";
+import styles from "./recordeditdetail.module.css";
+import ButtonCommon from "@components/UI/ButtonCommon";
+import { useState } from "react";
+
+interface Nutrition {
+  name : string,
+  gram : string 
+}
+
 const RecordEditDetail = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  let amount = 1;
+
+  const nutrients = [
+    { name: "탄수화물", gram: "0g" },
+    { name: "단백질", gram: "0g" },
+    { name: "지방", gram: "0g" },
+    { name: "식이섬유", gram: "0g" },
+  ];
+
   return (
-    <>
-      <div>식단별 정보</div>
-    </>
+    <div className={styles.container}>
+
+      <div className={styles.titlebox}>
+        <p className="s-large">음식명</p>
+        <p className="r-super" style={{marginLeft:"auto"}}>0Kcal</p>
+      </div>
+
+      <div className={styles.nutrientbox}>
+        {nutrients.map((nutrition: Nutrition, index) => (
+          <div key={index} className={styles.circle}>
+            <p className="s-regular">{nutrition.name}</p>
+            <p className="b-small" style={{color: "var(--main-blue)"}}>{nutrition.gram}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.searchbox}>
+        <p className="r-medium" style={{ color: "var(--main-skyblue)", textAlign: "left", marginBottom: "9px"}}>
+          음식을 수정하려면 검색하세요!
+        </p>
+        <div className={styles.searchform}>
+          <InputCommon
+            className="medium r-large default"
+            value={searchInput}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchInput(e.target.value)}
+          />
+          <ButtonCommon className="small b-small active">
+            수정
+          </ButtonCommon>
+        </div>
+      </div>
+
+      <div className={styles.calbox}>
+        <div className={styles.caltext}>
+          <p className="r-large">얼마나 먹었나요?</p>
+          <p className="r-super">{amount}g</p>
+        </div>
+        <div className={styles.calinput}>
+          <img className={styles.calbtn} src="/icons/minusicon.png" alt="-" />
+          <p className="s-big">{amount}</p>
+          <img className={styles.calbtn} src="/icons/plusicon.png" alt="+" />
+        </div>
+      </div>
+
+      <div className={styles.btnbox}>
+        <ButtonCommon className="medium b-small disabled">취소</ButtonCommon>
+        <ButtonCommon className="medium b-small active">수정 완료</ButtonCommon>
+      </div>
+    </div>
   )
 
 }
 
-export default RecordEditDetail;
+export default RecordEditDetail; 
