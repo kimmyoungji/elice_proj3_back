421e7fb27862bc66d290e41143a7a99bbb189bb9
hyunjinb <hyunjinb394@gmail.com>
2024-01-10 16:13:02 +0900

    Fix : Meal Page 하단 그래프 수정

--- src/components/pages/record/MealTag.tsx
+++ src/components/pages/record/MealTag.tsx
@@ -1,20 +1,32 @@
-import style from '@styles/record/recordToday.module.css';
+import style from '@styles/record/mealTag.module.css';
 
+interface MealTagProps {
+  className?: string;
+}
 
+const mealTagDetails = [
+  { mealDetail : ["떡국", "부", "간장", "만두", "나물","떡국", "부침개", "간장", "만두", "나물","떡국", "부침개", "간장", "만두", "나물"] },
+]
 
-const MealTag = ( ) => {
+const MealTag: React.FC<MealTagProps> = ({className}) => {
 
 
     return (
-    <>
-        
-
-
-      
-      
-    </>
-    )
-  };
+      <div className={className}>
+        <div className={style.containerName}> 먹은 음식 </div>
+        {mealTagDetails[0].mealDetail.length > 0 ? (
+        <div className={style.tagContainer}>
+          {mealTagDetails[0].mealDetail.map((food, index) => (
+            <span key={index} className={style.tagStyle}>{food}</span>
+          ))}
+        </div>
+      ) : (
+        <div className={style.guideComment}>오늘의 식단을 업로드 해주세요.</div>
+      )}
+    </div>
+  );
+};
   
   export default MealTag;
   
+
--- src/components/pages/record/NutritionAnalysis.tsx
+++ src/components/pages/record/NutritionAnalysis.tsx
@@ -1,16 +1,61 @@
-import style from '@styles/record/recordToday.module.css';
+import style from '@styles/record/nutritionAnalysis.module.css';
+import { BarChart } from './BarChart';
+import Bar from './Bar';
 
+interface NutritionAnalysisProps {
+    className?: string;
+  }
 
+interface MealData {
+    mealTime: string;
+    calories: number | string;
+    carbo: number | string;
+    fat: number | string;
+    fiber: number | string;
+  }
 
-const NutritionAnalysis = ( ) => {
+const nutritionData : MealData[] = [
+    { mealTime: "점심", calories : 1500 , carbo:  "", fat :  "", fiber:  ""}
+]
 
+const goalCalories = 1300
+
+const NutritionAnalysis: React.FC<NutritionAnalysisProps> = ({className} ) => {
+    
+    const totalCalories = nutritionData.reduce((sum, item) => {
+        const calories = typeof item.calories === 'number' ? item.calories : 0;
+        return sum + calories;
+      }, 0); 
+    const percentage = totalCalories === 0 ? 0 : Math.min(100, (totalCalories / goalCalories) * 100);
+    const barFill = percentage > 100 ? "#007bff" : "#ff6a6a"
 
     return (
     <>
-        
+        <div className={className}>
+        <div className={style.header}> 
+        <div className={style.title}>총섭취량</div>
+        <div className={style.caloriesCount}>
+        <div className={style.intakeCalories}> {nutritionData[0].calories || 0}  </div>
+        <div className={style.totalCalories}> /{goalCalories} </div>
+        </div>
+        </div>
+        <BarChart>
+                    <Bar
+                    key="goal-calories"
+                    width="100%"
+                    height="20px" 
+                    fill="#edf3f9" 
+                    />
 
+                    <Bar
+                    key="consumed-calories"
+                    width={`${percentage}%`}
+                    height="20px" 
+                    fill={barFill}
+                    />
+        </BarChart>
 
-      
+        </div>
       
     </>
     )
