cea660e14582e2a12809008609ef1334b4299f87
[AI9]허제인 <codingjamee@gmail.com>
2024-01-11 03:08:15 +0900

    Feat: getClassesArr util함수

---
+++ src/utils/getClassesArr.ts
@@ -0,0 +1,20 @@
+import { getClassNameType } from "typings/buttontypes";
+
+export const getClassNamesArr = (
+  classNames: string | undefined,
+  arr: any[]
+): string[] | string | undefined => {
+  console.log(arr);
+  let cls: string[] = [];
+  if (arr.length > 0) {
+    cls = cls?.concat(
+      arr.reduce((acc: string[], [key, val]: getClassNameType[]) => {
+        //val이 유효할 경우에만 key-value
+        val && acc.push(`${key.toString()}-${val?.toString()}`);
+        return acc;
+      }, [])
+    );
+    classNames && cls?.unshift(classNames.toString());
+  }
+  return cls;
+};
