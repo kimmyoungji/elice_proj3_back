f09553909cd32bae7257b504bf9efa14ca0be5e7
[AI9]허제인 <codingjamee@gmail.com>
2024-01-11 19:10:52 +0900

    Style: 프리티어 적용으로 코드 포맷 변경

--- src/components/pages/calendar/CalendarHeader.tsx
+++ src/components/pages/calendar/CalendarHeader.tsx
@@ -1,10 +1,10 @@
 //
-import { useId } from 'react';
-import { Toggle, ToggleButton } from '@components/UI/Toggle';
-import { Dropdown } from './dropdown/Dropdown';
-import DropdownButton from './dropdown/DropdownButton';
-import DropdownItem from './dropdown/DropdownItem';
-import { useCalendarContext } from './Calendar';
+import { useId } from "react";
+import { Toggle, ToggleButton } from "@components/UI/Toggle";
+import { Dropdown } from "./dropdown/Dropdown";
+import DropdownButton from "./dropdown/DropdownButton";
+import DropdownItem from "./dropdown/DropdownItem";
+import { useCalendarContext } from "./Calendar";
 
 const CalendarHeader = () => {
   const { yearMonth } = useCalendarContext();
@@ -14,9 +14,17 @@ const CalendarHeader = () => {
     console.log(value);
   };
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px', height: '40px', padding: '1px' }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        margin: "20px",
+        height: "40px",
+        padding: "1px",
+      }}
+    >
       <Dropdown onChange={handleChange}>
-        <DropdownButton>{yearMonth.split('-').join('. ')}</DropdownButton>
+        <DropdownButton>{yearMonth.split("-").join(". ")}</DropdownButton>
         {/* item은 현재 날짜에서 앞뒤로????? 인피니티 스크롤???...*/}
         <DropdownItem id={itemId}>aasasd</DropdownItem>
       </Dropdown>
--- src/components/pages/calendar/CalendarTitle.tsx
+++ src/components/pages/calendar/CalendarTitle.tsx
@@ -1,12 +1,12 @@
-import classes from './carlendartitle.module.css';
+import classes from "./carlendartitle.module.css";
 
 const CalendarTitle = () => {
   return (
     <div className={classes.title}>
-      <div className={classes['title-text']}>
+      <div className={classes["title-text"]}>
         <p>식단달력</p>
       </div>
-      <div className={classes['title-line']} />
+      <div className={classes["title-line"]} />
     </div>
   );
 };
