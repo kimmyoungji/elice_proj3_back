ba7172b2a5f338e7a746484cab7f4b2d307f9ea3
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-01-11 15:22:35 +0900

    Feature: UsersRepository Class 정의함.

---
+++ src/users/users.repository.ts
@@ -0,0 +1,16 @@
+import { DataSource, Repository } from "typeorm";
+import { User } from "./user.entity";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+
+@Injectable()
+export class UsersRepository extends Repository<User> {
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) {
+        super(userRepository.target, userRepository.manager, userRepository.queryRunner);
+    }
+    
+    async createUser(user: User): Promise<User> {
+        const newUser = this.create(user);
+        return await this.save(newUser);
+    }
+}
