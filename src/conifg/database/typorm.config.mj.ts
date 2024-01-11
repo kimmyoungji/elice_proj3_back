6d8fc7a65590230685f28193860addaf6fbeaab4
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-01-11 15:24:21 +0900

    Feature: typeORMConfig 파일 수정함

--- src/conifg/database/typorm.config.mj.ts
+++ src/conifg/database/typorm.config.mj.ts
@@ -1,5 +1,6 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import * as config from "config";
+import { User } from "src/users/user.entity";
 
 const dbConfig = config.get("db");
 
@@ -10,6 +11,6 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   username: process.env.RDS_USERNAME || dbConfig.username,
   password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
-  entities: [__dirname + "/../../**/*.entity.{js, ts}"],
+  entities: [User],
   synchronize: true,
 };
