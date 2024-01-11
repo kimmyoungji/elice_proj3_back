a23c059a21760136321c5e790ba530516be4c8e3
dev-vming <dev.vming@gmail.com>
2024-01-11 20:05:34 +0900

    Feat: AI 식단 확인 페이지 검색창 구현

--- src/components/pages/record/RecordEditDetail.tsx
+++ src/components/pages/record/RecordEditDetail.tsx
@@ -8,10 +8,20 @@ interface Nutrition {
   gram : string 
 }
 
+
 const RecordEditDetail = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [searching, setSearching] = useState(false);
+
+  const handleSearch = () => {
+    setSearching(true);
+    //api 호출 코드 
+    //특정항목 선택 시 setSearching(false)
+  }
 
   let amount = 1;
+  const searchResults = ["떡국", "두살 떡국","1등 떡국 최고" ,"세살 떡국", "네살 떡국", "다섯살 떡국","좋은 떡국","나쁜 떡국","해맑은 떡국"]; 
+  // const searchResults:any= undefined;
 
   const nutrients = [
     { name: "탄수화물", gram: "0g" },
@@ -49,9 +59,31 @@ const RecordEditDetail = () => {
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
               setSearchInput(e.target.value)}
           />
-          <ButtonCommon size="small" variant="default-active">
+          <ButtonCommon size="small" variant="default-active" onClickBtn={handleSearch}>
             검색
           </ButtonCommon>
+          {searching && 
+            <div className={styles.resultbox}>
+              <div style={{overflowY:"auto"}}>
+                {searchResults
+                  ? searchResults.map((result:string) =>
+                    <p className="r-medium" style={{marginBottom: "5px"}}>
+                      {result.split(searchInput)[0] !== "" && result.split(searchInput)[0]}
+                      <span className="r-medium" style={{color: "var(--main-blue)"}}>{searchInput}</span>
+                      {result.split(searchInput)[1] !== "" && result.split(searchInput)[1]}
+                    </p>
+                    )
+                  : <p className="r-medium" style={{textAlign:"center", marginTop: "60px"}}>검색 결과가 없습니다.</p>}
+              </div>
+              
+              <p
+                className="r-medium"
+                style={{ color: "var(--main-skyblue)", marginLeft: "auto", marginTop: "auto", paddingTop: "10px"}}
+                onClick={()=>setSearching(false)}
+              >닫기</p>
+            </div>
+          }
+          
         </div>
       </div>
 
