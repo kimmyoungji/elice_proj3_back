727a0bc5c0441d0542f6fed3a3faa7e568ecc421
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-01-11 15:21:40 +0900

    Feature: Users 모듈에 UsersService, UsersRepository 프로바이더로 등록, User테이블과 연결된 TypeORM모듈을  Users 모듈에 import함

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
+import { UsersRepository } from './users.repository';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository]
 })
 export class UsersModule {}
