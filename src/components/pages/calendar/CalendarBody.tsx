e6d2faa85d6a25e40a8c62f373285f0102c5d312
[AI9]허제인 <codingjamee@gmail.com>
2024-01-11 13:41:31 +0900

    Style: prettier적용으로 코드 포맷 변경

--- src/components/pages/calendar/CalendarBody.tsx
+++ src/components/pages/calendar/CalendarBody.tsx
@@ -1,32 +1,54 @@
-import { ReactElement } from 'react';
-import classes from './calendarbody.module.css';
-import { useCalendarContext } from './Calendar';
+import { ReactElement } from "react";
+import classes from "./calendarbody.module.css";
+import { useCalendarContext } from "./Calendar";
 
 const CalendarBody = () => {
   const { yearMonth: selectedYearMonth } = useCalendarContext();
   //비어있을 수 없음 초깃값 이번해.이번달
 
-  const [thisYear, thisMonth] = selectedYearMonth.split('.');
-  const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+  const [thisYear, thisMonth] = selectedYearMonth.split(".");
+  const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
   const dayOfWeekArray = dayOfWeek.map((day, idx) => (
-    <div className={classes['day-wrapper']} key={`day-${idx}`}>
-      <p className={`${classes['day-arr']} r-big`}>{day}</p>
+    <div className={classes["day-wrapper"]} key={`day-${idx}`}>
+      <p className={`${classes["day-arr"]} r-big`}>{day}</p>
     </div>
   ));
 
   const getThisMonthArray = () => {
-    const thisMonthTotal = new Date(parseInt(thisYear), parseInt(thisMonth), 0).getDate();
-    const thisMonthFirstDay = new Date(parseInt(thisYear), parseInt(thisMonth) - 1, 1).getDay();
+    const thisMonthTotal = new Date(
+      parseInt(thisYear),
+      parseInt(thisMonth),
+      0
+    ).getDate();
+    const thisMonthFirstDay = new Date(
+      parseInt(thisYear),
+      parseInt(thisMonth) - 1,
+      1
+    ).getDay();
     const arr: ReactElement[] = [];
     const totalCalNum = thisMonthTotal + thisMonthFirstDay > 35 ? 42 : 35;
     new Array(totalCalNum).fill(0).map((_, idx) => {
-      if (idx < thisMonthFirstDay || idx >= thisMonthTotal + thisMonthFirstDay) {
-        return arr.push(<div key={`$cal-${idx}`} className={`${classes['cal-circle']} b-small`}></div>);
-      }
-      if (idx >= thisMonthFirstDay && idx <= thisMonthTotal + thisMonthFirstDay) {
+      if (
+        idx < thisMonthFirstDay ||
+        idx >= thisMonthTotal + thisMonthFirstDay
+      ) {
         return arr.push(
-          <div key={`$cal-${idx}`} className={`${classes['cal-circle']} b-small`}>
+          <div
+            key={`$cal-${idx}`}
+            className={`${classes["cal-circle"]} b-small`}
+          ></div>
+        );
+      }
+      if (
+        idx >= thisMonthFirstDay &&
+        idx <= thisMonthTotal + thisMonthFirstDay
+      ) {
+        return arr.push(
+          <div
+            key={`$cal-${idx}`}
+            className={`${classes["cal-circle"]} b-small`}
+          >
             {idx - thisMonthFirstDay + 1}
           </div>
         );
@@ -38,10 +60,10 @@ const CalendarBody = () => {
   console.log(getThisMonthArray());
 
   return (
-    <div className={classes['cla-body']}>
-      <div className={classes['cla-week']}>{dayOfWeekArray}</div>
-      <div className={classes['cla-week']}>
-        <div className={classes['cal-flex']}>{getThisMonthArray()}</div>
+    <div className={classes["cla-body"]}>
+      <div className={classes["cla-week"]}>{dayOfWeekArray}</div>
+      <div className={classes["cla-week"]}>
+        <div className={classes["cal-flex"]}>{getThisMonthArray()}</div>
       </div>
     </div>
   );
