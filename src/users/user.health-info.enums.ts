a093a445b5e5207f0f8f566d62dd8943e64a34c9
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-01-11 15:25:04 +0900

    Feature: user_MVP service 코드 작성 시작

---
+++ src/users/user.health-info.enums.ts
@@ -0,0 +1,18 @@
+export enum ActivityAmount {
+  Low = 1,
+  Medium = 2,
+  High = 3,
+  VeryHigh = 4,
+}
+
+export enum Gender {
+  MALE = 1,
+  FEMAEL = 2,
+  OTHER = 3,
+}
+
+export enum DietGoal {
+  MUSCLE_GAIN = 1,
+  WEIGHT_LOSS = 2,
+  MAINTENANCE = 3,
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,49 @@
+import { ActivityAmount } from './user.health-info.enums';
+import { DietGoal, Gender } from './user.health-info.enums';
+import { DataSource } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository} from './users.repository';
+import { SignUpUserDto } from './dto/signUpUpser.dto';
+import { v4 as uuidv4} from 'uuid';
+import { User } from './user.entity';
+import { HealthInfo } from './health-info.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+    ) {}
+
+    async signUp(signUpUserDto: SignUpUserDto) {
+
+        console.log(Gender[signUpUserDto.gender], typeof Gender[signUpUserDto.gender]);
+        console.log(DietGoal[signUpUserDto.goal], typeof DietGoal[signUpUserDto.goal]);
+        console.log(ActivityAmount[signUpUserDto.activity], typeof ActivityAmount[signUpUserDto.activity]);
+        console.log(Gender['FEMAEL'])
+
+        // signUpUserDto -> User Entity Type
+        const newUser = new User();
+        newUser.user_id = uuidv4();
+        newUser.email = signUpUserDto.email;
+        newUser.username = signUpUserDto.username;
+        newUser.password = signUpUserDto.password;
+        newUser.birthday = signUpUserDto.birthday;
+        newUser.gender = signUpUserDto.gender;
+
+        // signUpUserDto -> HealthInfo Entity Type
+        const newHealthInfo = new HealthInfo();
+        newHealthInfo.user_id = newUser.user_id;
+        newHealthInfo.weight = signUpUserDto.weight;
+        newHealthInfo.height = signUpUserDto.height;
+        newHealthInfo.goal = signUpUserDto.goal;
+        newHealthInfo.target_weight = signUpUserDto.targetWeight;
+        newHealthInfo.target_calories = signUpUserDto.targetCalories;
+        newHealthInfo.activity_amount = signUpUserDto.activity;
+
+        const result1 = await this.usersRepository.createUser(newUser);
+        // const result2 = await this.usersRepository.createHealthInfo(newHealthInfo);
+        return { result1, newHealthInfo };
+
+    }
+}
