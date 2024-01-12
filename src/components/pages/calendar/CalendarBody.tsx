79adbbf9e84885da46f57534f040fc6ee490a25c
[AI9]허제인 <codingjamee@gmail.com>
2024-01-12 11:01:31 +0900

    Refactor: createElement방식으로 수정

--- src/components/pages/calendar/CalendarBody.tsx
+++ src/components/pages/calendar/CalendarBody.tsx
@@ -1,7 +1,8 @@
-import { ReactElement } from "react";
+import { ReactElement, SyntheticEvent, createElement } from "react";
 import classes from "./calendarbody.module.css";
 import { useCalendarContext } from "./Calendar";
 import Album from "./Album";
+import ButtonCommon from "@components/UI/ButtonCommon";
 
 //필요한 전역 정보 나의 목표칼로리 (유저 정보에 포함)
 const DUMMYCumulative_cal_Date = {
@@ -12,9 +13,16 @@ const DUMMYCumulative_cal_Date = {
 const DUMMYtargetCalories = 1200;
 
 const CalendarBody = () => {
-  const { yearMonth: selectedYearMonth, isAlbum } = useCalendarContext();
+  const {
+    thisYear,
+    thisMonth,
+    selectedIndex,
+    setSelectedIndex,
+    setThisMonth,
+    setThisYear,
+    isAlbum,
+  } = useCalendarContext();
 
-  const [thisYear, thisMonth] = selectedYearMonth.split(".");
   const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 
   const dayOfWeekArray = dayOfWeek.map((day, idx) => (
@@ -23,78 +31,80 @@ const CalendarBody = () => {
     </div>
   ));
 
+  const onClickDate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const clickedElementId = Number(
+      (e.target as HTMLDivElement).id.split("-")[1]
+    );
+    console.log(clickedElementId);
+    setSelectedIndex(clickedElementId);
+  };
+
   const getThisMonthArray = () => {
-    const thisMonthTotal = new Date(
-      parseInt(thisYear),
-      parseInt(thisMonth),
-      0
-    ).getDate();
-    const thisMonthFirstDay = new Date(
-      parseInt(thisYear),
-      parseInt(thisMonth) - 1,
-      1
-    ).getDay();
+    const thisMonthTotal = new Date(thisYear, thisMonth, 0).getDate();
+    const thisMonthFirstDay = new Date(thisYear, thisMonth - 1, 1).getDay();
     const monthArr: ReactElement[] = [];
     const totalCalNum = thisMonthTotal + thisMonthFirstDay > 35 ? 42 : 35;
+    const getDayNumber = (idx: number): number => {
+      return idx - thisMonthFirstDay + 1;
+    };
 
     new Array(totalCalNum).fill(0).map((_, idx) => {
-      //숫자 없는 것
-      if (
-        idx < thisMonthFirstDay ||
-        idx >= thisMonthTotal + thisMonthFirstDay
-      ) {
-        return monthArr.push(
-          <div
-            key={`$cal-${idx}`}
-            className={`${classes["cal-circle"]} b-small`}
-          ></div>
-        );
-      }
-      //숫자 있는 것
-      if (
-        idx >= thisMonthFirstDay &&
-        idx <= thisMonthTotal + thisMonthFirstDay
-      ) {
-        //해당 날짜에 입력한 칼로리 데이터 존재한다면
-        if (
-          DUMMYCumulative_cal_Date["existedDate"].includes(
-            idx - thisMonthFirstDay + 1
-          )
-        ) {
-          const extedDateIndex = DUMMYCumulative_cal_Date[
-            "existedDate"
-          ].findIndex((el) => el === idx - thisMonthFirstDay + 1);
-          //목표 칼로리 비교 색깔 구분 적당 vs 과식
-          const colorCls =
-            DUMMYCumulative_cal_Date["totalCalData"][extedDateIndex] >
-            DUMMYtargetCalories
-              ? "over-eat"
-              : "moderate";
-          monthArr.push(
-            <div className={classes["day-wrapper"]} key={`$cal-${idx}`}>
-              <div className={`${classes[`${colorCls}`]} b-small`}>
-                {idx - thisMonthFirstDay + 1}
-              </div>
-              <p
-                className={`r-regular ${
+      const existedDateIndex = DUMMYCumulative_cal_Date[
+        "existedDate"
+      ].findIndex((el) => el === getDayNumber(idx));
+      //목표 칼로리 비교 색깔 구분 적당 vs 과식
+      const colorCls =
+        DUMMYCumulative_cal_Date["totalCalData"][existedDateIndex] >
+        DUMMYtargetCalories
+          ? "over-eat"
+          : "moderate";
+
+      monthArr.push(
+        createElement("div", {
+          key: `$cal-${idx}`,
+          className:
+            idx >= thisMonthFirstDay && idx < thisMonthTotal + thisMonthFirstDay
+              ? `${classes["day-wrapper"]} b-small`
+              : ` ${classes["cal-circle"]} b-small`,
+          children: [
+            //날짜에 맞는 idx일때만 숫자를 넣음
+            createElement("div", {
+              key: `$date-${idx}`,
+              className:
+                //칼로리데이터 존재하는 날짜의 인덱스
+                DUMMYCumulative_cal_Date["existedDate"].includes(
+                  getDayNumber(idx)
+                )
+                  ? `${classes[`${colorCls}`]} b-small`
+                  : `${classes["cal-circle"]} b-small`,
+              //id는 날짜범위 내에서만
+              id:
+                idx >= thisMonthFirstDay &&
+                idx < thisMonthTotal + thisMonthFirstDay
+                  ? `idx-${getDayNumber(idx)}`
+                  : null,
+              onClick: onClickDate,
+              children:
+                idx >= thisMonthFirstDay &&
+                idx < thisMonthTotal + thisMonthFirstDay &&
+                getDayNumber(idx),
+            }),
+            //데이터 존재하는 idx에서만
+            DUMMYCumulative_cal_Date["existedDate"].includes(
+              getDayNumber(idx)
+            ) &&
+              createElement("p", {
+                key: `$p-${idx}`,
+                className: `r-regular ${
                   colorCls === "over-eat"
                     ? classes[`font-over-eat`]
                     : classes[`font-moderate`]
-                }`}
-              >{`+${DUMMYCumulative_cal_Date["totalCalData"][extedDateIndex]}`}</p>
-            </div>
-          );
-        } else {
-          //칼로리 데이터 없는 날
-          monthArr.push(
-            <div className={classes["day-wrapper"]} key={`$cal-${idx}`}>
-              <div className={`${classes["cal-circle"]} b-small`}>
-                {idx - thisMonthFirstDay + 1}
-              </div>
-            </div>
-          );
-        }
-      }
+                }`,
+                children: `+${DUMMYCumulative_cal_Date["totalCalData"][existedDateIndex]}`,
+              }),
+          ],
+        })
+      );
       return monthArr;
     });
     return monthArr;
@@ -103,12 +113,22 @@ const CalendarBody = () => {
   return (
     <>
       {!isAlbum && (
-        <div className={classes["cla-body"]}>
-          <div className={classes["cla-week"]}>{dayOfWeekArray}</div>
-          <div className={classes["cla-week"]}>
-            <div className={classes["cal-flex"]}>{getThisMonthArray()}</div>
+        <>
+          <div className={classes["cla-body"]}>
+            <div className={classes["cla-week"]}>{dayOfWeekArray}</div>
+            <div className={classes["cla-week"]}>
+              <div className={classes["cal-flex"]}>{getThisMonthArray()}</div>
+            </div>
           </div>
-        </div>
+          <ButtonCommon
+            style={{ margin: "20px auto" }}
+            variant="default-active"
+            size="big"
+            disabled={true}
+          >
+            선택한 날짜로 이동
+          </ButtonCommon>
+        </>
       )}
       {isAlbum && <Album />}
     </>
