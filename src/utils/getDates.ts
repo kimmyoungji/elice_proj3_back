5484a981900a416a45ee78bf4676c2f72d738ce1
[AI9]허제인 <codingjamee@gmail.com>
2024-01-12 11:03:48 +0900

    Style: 코드 포맷팅 변경

--- src/utils/getDates.ts
+++ src/utils/getDates.ts
@@ -2,24 +2,31 @@ const getDates = () => {
   const date = new Date();
   const thisYear = date.getFullYear();
   const thisMonthNum = date.getMonth() + 1;
-  const thisMonthwithzero = thisMonthNum >= 10 ? thisMonthNum : `0${thisMonthNum}`;
-  const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+  const thisMonthwithzero =
+    thisMonthNum >= 10 ? thisMonthNum : Number(`0${thisMonthNum}`);
+  const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
   const thisDay = date.getDay();
   const thisDayText = dayOfWeek[thisDay];
   const thisHour = date.getHours();
 
   const getNowMealText = () => {
     if (thisHour >= 5 && thisHour < 11) {
-      return '아침';
+      return "아침";
     } else if (thisHour >= 11 && thisHour < 17) {
-      return '점심';
+      return "점심";
     } else if (thisHour >= 17 && thisHour < 23) {
-      return '저녁';
+      return "저녁";
     } else {
-      return '간식';
+      return "간식";
     }
   };
 
-  return { thisYear, thisMonth: thisMonthwithzero, thisDay, thisDayText, getNowMealText };
+  return {
+    thisYear,
+    thisMonth: thisMonthwithzero,
+    thisDay,
+    thisDayText,
+    getNowMealText,
+  };
 };
 export default getDates;
