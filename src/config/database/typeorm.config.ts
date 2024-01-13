c2d87a1fb263a973dd0d3ef1b62711886e623560
Jaeung <wkdwodnd1234@gmail.com>
2024-01-13 18:03:56 +0900

    Fix: config 수정

--- src/config/database/typeorm.config.ts
+++ src/config/database/typeorm.config.ts
@@ -1,32 +1,17 @@
-// import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-// import * as config from "config";
-
-// const dbConfig = config.get("db");
-
-// export const typeORMConfig: TypeOrmModuleOptions = {
-//   type: dbConfig.type,
-//   host: process.env.RDS_HOSTNAME || dbConfig.host,
-//   port: process.env.RDS_PORT || dbConfig.post,
-//   username: process.env.RDS_USERNAME || dbConfig.username,
-//   password: process.env.RDS_PASSWORD || dbConfig.password,
-//   database: process.env.RDS_DB_NAME || dbConfig.database,
-//   entities: [__dirname + "/../../**/*.entity.{js, ts}"],
-//   synchronize: true,
-// };
-
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-
+import * as config from "config";
 import { Record } from "src/record/record.entity";
 
-export const typeORMConfig : TypeOrmModuleOptions = {
-   type: 'postgres',
-   host: 'localhost',
-   port: 5432,
-   username: 'postgres',
-   password: '0000',
-   database: 'ggu',
+const dbConfig = config.get("db");
 
-   entities: [Record],
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.post,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
+  entities: [Record],
+  synchronize: false,
+};
 
-   synchronize: true
-}
