a9475e2a377c0fcae8eee4f677aa0c70fc04fb8e
iris2598 <iris2598@naver.com>
2024-01-16 17:45:50 +0900

    Fix: className 에러 수정

--- src/components/pages/home/Week.tsx
+++ src/components/pages/home/Week.tsx
@@ -82,12 +82,17 @@ const Week = () => {
       <div className={`${styles.select_wrapper} b-small`}>
         <p>{nowYear}.{String(nowMonth + 1).padStart(2, "0")}</p>
         <div onClick={onToggle} className={styles.dropdown_wrapper}>
-          {selectedWeek}주차 ▼
+          <div className={styles.dropdown_line}>
+          {selectedWeek}주차 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5" width="17">
+  <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+</svg>
+          </div>
+
           {
             isOpen && (
               <div className={styles.dropdown}>
               {new Array(totalWeek).fill(null).map((_, idx)=>(
-                <div key={`week-${idx}`} onClick={()=>onClickWeek(idx+1)} className={idx+1 === selectedWeek && styles.active}>{idx+1}주차</div>
+                <div key={`week-${idx}`} onClick={()=>onClickWeek(idx+1)} className={idx+1 === selectedWeek ? styles.active : ""}>{idx+1}주차</div>
               ))}
               </div>
             )
