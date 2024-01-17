b4c81d0cc8e08d97742de536858fc9f5232808f2
Jaeung <wkdwodnd1234@gmail.com>
2024-01-17 09:34:12 +0900

    Feat: DTO 설정

--- src/record/record.repository.ts
+++ src/record/record.repository.ts
@@ -1,26 +1,31 @@
-import { DataSource, LessThanOrEqual, MoreThanOrEqual, Repository } from "typeorm";
-import { Record } from "./record.entity";
+import { DataSource, Equal, LessThanOrEqual, MoreThanOrEqual, Repository } from "typeorm";
+import { MealType, Record } from "./record.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 // import { User } from "src/user/user.entity";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { response } from "express";
 
 @Injectable()
 export class RecordRepository extends Repository<Record> {
-//   constructor(
-//     @InjectRepository(Record) private dataSource: DataSource
-//   ) {
-//     super(Record, dataSource.manager);
-//   }
 
 constructor(@InjectRepository(Record) private recordRepository: Repository<Record>) {
   super(recordRepository.target, recordRepository.manager, recordRepository.queryRunner);
   }
 
   // 식단 조회
-  async findByDate(date: Date): Promise<Record[]> {
+  async findByDate(date: string, mealType: string): Promise<Record[]> {
+    const dateObj = new Date(date);
+    const mealTypeEnum = MealType[mealType as keyof typeof MealType]
+
+    if (!mealTypeEnum) {
+      throw new BadRequestException(`Invalid mealType: ${mealType}`);
+    }
+
     const records = await this.recordRepository.find({
-      where: { updatedDate: date },
+      where: { 
+        firstRecordDate: LessThanOrEqual(dateObj),
+        mealType: mealTypeEnum 
+       },
     });
     return records;
   }
@@ -29,7 +34,7 @@ constructor(@InjectRepository(Record) private recordRepository: Repository<Recor
   async createRecord(record: {
     recordId: string,
     userId: string,
-    mealType: string,
+    mealType: MealType,
     foodInfoId: string,
     foodCounts: number,
     carbohydrates?: number,
@@ -48,14 +53,19 @@ constructor(@InjectRepository(Record) private recordRepository: Repository<Recor
   // 식단수정
   async updateRecord(date: string, mealType: string, updateData: Partial<Record>): Promise<Record> {
     const dateObj = new Date(date);
+    const mealTypeEnum = MealType[mealType as keyof typeof MealType];
+
+    if (!mealTypeEnum) {
+      throw new BadRequestException(`잘못된 식사 유형: ${mealType}`);
+    }
     const record = await this.recordRepository.findOne({
       where: { 
-        firstRecordDate: LessThanOrEqual(dateObj),
-        mealType: mealType
+        firstRecordDate: Equal(dateObj),
+        mealType: mealTypeEnum 
       },
     });
     if (!record) {
-      throw new NotFoundException(`Record not found for the date: ${date} and mealType: ${mealType}`);
+      throw new NotFoundException(`해당 날짜 레코드가 없거나 식사 유형이 옳지 않습니다`);
     }
     this.recordRepository.merge(record, updateData);
     record.updatedDate = new Date();
@@ -66,14 +76,14 @@ constructor(@InjectRepository(Record) private recordRepository: Repository<Recor
   
   // 식단 삭제
   async deleteRecord(date: string, mealType: string): Promise<void> {
+    const mealTypeEnum = MealType[mealType as keyof typeof MealType];
     const dateObj = new Date(date);
     const result = await this.recordRepository.delete({
       firstRecordDate: LessThanOrEqual(dateObj),
-      updatedDate: MoreThanOrEqual(dateObj),
-      mealType: mealType
+      mealType: mealTypeEnum 
     });
     if (result.affected === 0) {
-      throw new NotFoundException(`Record not found for the date: ${date} and mealType: ${mealType}`);
+      throw new NotFoundException(`해당 날짜 레코드가 없거나 식사 유형이 옳지 않습니다`);
     }
   }
   
