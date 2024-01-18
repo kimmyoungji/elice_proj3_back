3c087f5df6c68730be00f47c165bd933da343ddf
[AI9]허제인 <codingjamee@gmail.com>
2024-01-19 01:16:50 +0900

    Fix: 적용 테스트 코드 삭제

--- src/components/pages/calendar/Calendar.tsx
+++ src/components/pages/calendar/Calendar.tsx
@@ -1,13 +1,8 @@
 import CarlendarHeader from './CalendarHeader';
 import CalendarBody from './CalendarBody';
-import CalendarTitle from './CalendarTitle';
-import { createContext, useEffect } from 'react';
+import { createContext } from 'react';
 import { Dispatch, ReactElement, useContext, useState } from 'react';
 import getDates from '../../../utils/getDates';
-import Toast from '@components/UI/Toast';
-import ToastText from '@components/UI/ToastText';
-import ButtonCommon from '@components/UI/ButtonCommon';
-import InputNumber from '@components/UI/InputNumber';
 
 const CalendarContext = createContext<
   | {
@@ -78,52 +73,8 @@ export const useCalendarContext = () => {
 };
 
 const Calendar = () => {
-  const [showToast, setShowToast] = useState(false);
-  const [position, setPosition] = useState({
-    x: window.innerWidth / 2,
-    y: window.innerHeight / 2,
-  });
-
-  const showMeToast = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setShowToast(true);
-    console.log({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
-    console.log({ x: e.clientX, y: e.clientY });
-    setPosition((prev) => ({ ...prev, x: e.clientX, y: e.clientY }));
-  };
-  const [value, setValue] = useState(1);
-  const [value2, setValue2] = useState(1);
-
-  const valueChangeFn: React.ChangeEventHandler<HTMLInputElement> = (value) => {
-    value && setValue(Number(value));
-  };
-  const valueChangeFn2: React.ChangeEventHandler<HTMLInputElement> = (
-    value
-  ) => {
-    value2 && setValue2(Number(value));
-  };
-
-  useEffect(() => {
-    console.log(value);
-  }, [value]);
-
   return (
     <>
-      <ButtonCommon onClickBtn={showMeToast}>Button</ButtonCommon>
-      <Toast show={showToast} setShow={setShowToast} position={position}>
-        <ToastText>hello</ToastText>
-      </Toast>
-
-      <InputNumber
-        value={value}
-        onValueChange={valueChangeFn}
-        maxiumValue={23}
-      />
-      <InputNumber
-        value={value2}
-        onValueChange={valueChangeFn2}
-        maxiumValue={23}
-      />
-
       <CalendarProvider>
         <CarlendarHeader />
         <CalendarBody />
