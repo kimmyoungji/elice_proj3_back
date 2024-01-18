18dcd6ed48b6f006b35f0a4cbccd2b660d90af89
hyunjinb <hyunjinb394@gmail.com>
2024-01-18 16:05:14 +0900

    Fix : record edit으로 넘겨주는 state 수정

--- src/components/pages/record/RecordEdit.tsx
+++ src/components/pages/record/RecordEdit.tsx
@@ -17,18 +17,16 @@ interface MealTime {
 const RecordEdit = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
-
   const params = useParams();
   const date = params.date;
   const mealTime = params.mealTime;
-  const dateSplit = date?.split("-");
+  const dateSplit = date?.split('-');
   const mealTimetoStr: MealTime = {
-    "1": "아침",
-    "2": "점심",
-    "3": "저녁",
-    "4": "간식",
+    '1': '아침',
+    '2': '점심',
+    '3': '저녁',
+    '4': '간식',
   };
-  
 
   const [foods, setFoods] = useState([
     {
@@ -48,19 +46,19 @@ const RecordEdit = () => {
     },
   ]);
 
-  useEffect(() => {
-    if (state) {
-      let newFoods:Food[] = [];
-      state.map((tag:string) => (
-        newFoods.push({
-          foodName: tag,
-          foodImage: '/images/9gram_logo.png',
-          XYCoordinate: [0, 0],
-        })
-      ))
-      setFoods(newFoods);
-      }      
-  },[])
+  // useEffect(() => {
+  //   if (state) {
+  //     let newFoods:Food[] = [];
+  //     state.map((tag:string) => (
+  //       newFoods.push({
+  //         foodName: tag,
+  //         foodImage: '/images/9gram_logo.png',
+  //         XYCoordinate: [0, 0],
+  //       })
+  //     ))
+  //     setFoods(newFoods);
+  //     }
+  // },[])
 
   const [focus, setFocus] = useState<string | undefined | null>('');
 
@@ -92,7 +90,7 @@ const RecordEdit = () => {
       const delete_target = e.currentTarget.id;
       setFoods(foods.filter((food) => food.foodName !== delete_target));
     } else {
-      alert("음식은 1개 이상 등록되어야 합니다!")
+      alert('음식은 1개 이상 등록되어야 합니다!');
     }
   };
 
@@ -104,22 +102,22 @@ const RecordEdit = () => {
     el.scrollTo({
       left: el.scrollLeft + deltaY,
       behavior: 'smooth',
-    });    
+    });
   };
 
   const handleEnter = () => {
-    const main = document.querySelector(".main") as HTMLElement;
+    const main = document.querySelector('.main') as HTMLElement;
     main.style.overflow = 'hidden';
-  }
+  };
 
   const handleLeave = () => {
-    const main = document.querySelector(".main") as HTMLElement;
+    const main = document.querySelector('.main') as HTMLElement;
     main.style.overflow = 'scroll';
-  }
+  };
 
   const canvasRef = useRef<null[] | HTMLCanvasElement[]>([]);
 
-  const createCanvas = (food:Food, index:number) => {
+  const createCanvas = (food: Food, index: number) => {
     const canvas = canvasRef.current[index];
     const context = canvas?.getContext('2d');
 
@@ -127,22 +125,35 @@ const RecordEdit = () => {
     image.src = food.foodImage;
 
     image.onload = () => {
-      context?.drawImage(image, food.XYCoordinate[0], food.XYCoordinate[1], 90, 90, 0, 0, 90, 90);
+      context?.drawImage(
+        image,
+        food.XYCoordinate[0],
+        food.XYCoordinate[1],
+        90,
+        90,
+        0,
+        0,
+        90,
+        90
+      );
     };
-  }
+  };
 
   useEffect(() => {
     foods.map((food, index) => {
       return createCanvas(food, index);
-    })
+    });
   }, [foods]);
 
   return (
     <>
       <div className={styles.datebox}>
-        {dateSplit && mealTime &&
+        {dateSplit && mealTime && (
           <p className='b-small'>
-            {dateSplit[0]}년 {dateSplit[1]}월 {dateSplit[2]}일 {mealTimetoStr[mealTime]}</p>}
+            {dateSplit[0]}년 {dateSplit[1]}월 {dateSplit[2]}일{' '}
+            {mealTimetoStr[mealTime]}
+          </p>
+        )}
       </div>
 
       <div className={styles.imgbox}>
@@ -192,7 +203,9 @@ const RecordEdit = () => {
                       focus === food.foodName && styles.focusimg
                     }`}
                     id={food.foodName}
-                    ref={(element) => {canvasRef.current[index]= element}}
+                    ref={(element) => {
+                      canvasRef.current[index] = element;
+                    }}
                     width={90}
                     height={90}
                     onClick={(e) => handleFocus(e)}
@@ -227,7 +240,11 @@ const RecordEdit = () => {
         />
       )}
       <div className={styles.btnbox}>
-        <ButtonCommon size='medium' variant='disabled' onClick={()=>setFocus('')}>
+        <ButtonCommon
+          size='medium'
+          variant='disabled'
+          onClick={() => setFocus('')}
+        >
           취소
         </ButtonCommon>
         <ButtonCommon size='medium' variant='default-active'>
