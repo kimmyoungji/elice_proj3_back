c858df1607c0db9651f2d52a1376e7b32931ec33
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-01-21 16:40:43 +0900

    Revert "Fix: drop mock-front"
    This reverts commit d43c9ee90c40c0123b1a86ce71602711fcf7882c.
    
    retrive mock-front

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,71 +1,11 @@
-import { EntityManager, InsertResult } from "typeorm";
-import { User } from "./entities/user.entity";
-import { HealthInfo } from "./entities/health-info.entity";
+import { Injectable } from "@nestjs/common";
+import { DataSource, Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/user/user.entity";
 
-export class UserRepository{
-
-    // Create
-    public async saveUser(user: User, manager: EntityManager): Promise<InsertResult>{
-        try{
-            return await manager.createQueryBuilder(User, "user").insert()
-                                .into(User,['userId','email','password','providerId','username']).values(user).execute(); 
-        }catch(err){
-            throw err;
-        }
-       
-    }
-
-    // Read
-    public async findUserByEmail(email: string, manager: EntityManager): Promise<User>{
-        try{
-            return await manager.createQueryBuilder(User, "user").where("email = :email", {email}).getOne();
-        }catch(err){
-            throw err;
-        }
-    }
-
-    public async findUserAndHealthInfoByUserId(userId: string, manager: EntityManager){
-        try{
-            return await manager.createQueryBuilder(User, "user").leftJoinAndSelect("user.healthInfo", "healthInfo").where("user.user_id = :userId", {userId}).getOne();
-        }catch(err){
-            throw err;
-        }
-    }
-
-    public async findUserByUserId(userId: string, manager: EntityManager): Promise<User>{
-        try{
-            return await manager.createQueryBuilder(User, "user").select().where("user_id = :userId",{userId}).getOne();
-        }catch(err){
-            throw err;
-        }
-    }
-
-    // Update
-    public async updateUserByUserId(userId: string, user: User, manager: EntityManager){
-        try{
-            console.log("업데이트 될것", user);
-            return await manager.createQueryBuilder(User, "user").update(User).set(user).where("user_id = :userId",{userId}).execute();
-        }catch(err){
-            throw err;
-        }
-    }
-
-    public async updateHealthInfoIdByUserId(userId: string, healthInfo: HealthInfo, manager: EntityManager){
-        try{
-            return await manager.createQueryBuilder(User, "user").leftJoin('user.healthInfo', "healthInfo").update(User).set({healthInfo}).where("user_id = :userId",{userId}).execute();
-        }catch(err){
-            throw err;
-        }
-    }
-
-    // soft Delete
-    public async softDeleteUserByUserId(userId: string, manager: EntityManager){       
-        try{
-            console.log(userId);
-            return await manager.createQueryBuilder(User, "user").softDelete().where("user_id = :userId",{userId}).execute();
-        }catch(err){
-            throw err;
-        }
-    }
-
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(@InjectRepository(User) private dataSource: DataSource) {
+    super(User, dataSource.manager);
+  }
 }
