0155f60587458755d774625b31389ad0f4b94136
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-01-23 18:41:25 +0900

    Fix: user_mvp mock-front과 back  폴더 제거. master와 폴더 구조 일치시키기

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,65 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { setupSwagger } from "src/util/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { setupSwagger } from './util/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from "dotenv";
+import * as session from 'express-session';
+import * as passport from 'passport';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
+import express from 'express';
+
+dotenv.config({path: __dirname + '/../.env'});
 
 async function bootstrap() {
+  
+  // app 생성
   const app = await NestFactory.create(AppModule);
+  
+  // 세션 설정
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: 60 * 60 * 1000, // 1시간 동안 세션이 유효하도록 설정
+      },
+      name: process.env.SESSION_COOKIE_NAME,
+    }),
+  );
 
+  // passport 설정
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // swagger 설정
   setupSwagger(app);
+  
+  //cookie 설정
+  app.use(cookieParser(process.env.SESSION_SECRET));
+  
+  // api 프리픽스 설정
+  // app.setGlobalPrefix('api');
+  
+  // dto 유효성 검사 설정
+  app.useGlobalPipes(new ValidationPipe({ 
+    // whitelist: true, // dto에 없는 property는 거름, 200 OK
+    // transform: true, // string -> number, 200 OK
+    forbidNonWhitelisted: true, // dto에 없는 property는 거름, 400 에러
+    transformOptions: {
+      enableImplicitConversion: true, // string -> number, 암묵적 형변환 200 OK
+    } 
+  }));
 
+  // cors 설정
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }));
+
+  
+  // app 실행
   await app.listen(5001);
 }
 bootstrap();
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,11 +1,71 @@
-import { Injectable } from "@nestjs/common";
-import { DataSource, Repository } from "typeorm";
-import { InjectRepository } from "@nestjs/typeorm";
-import { User } from "src/user/user.entity";
+import { EntityManager, InsertResult } from "typeorm";
+import { User } from "./entities/user.entity";
+import { HealthInfo } from "./entities/health-info.entity";
 
-@Injectable()
-export class UserRepository extends Repository<User> {
-  constructor(@InjectRepository(User) private dataSource: DataSource) {
-    super(User, dataSource.manager);
-  }
+export class UserRepository{
+
+    // Create
+    public async saveUser(user: User, manager: EntityManager): Promise<InsertResult>{
+        try{
+            return await manager.createQueryBuilder(User, "user").insert()
+                                .into(User,['userId','email','password','providerId','username']).values(user).execute(); 
+        }catch(err){
+            throw err;
+        }
+       
+    }
+
+    // Read
+    public async findUserByEmail(email: string, manager: EntityManager): Promise<User>{
+        try{
+            return await manager.createQueryBuilder(User, "user").where("email = :email", {email}).getOne();
+        }catch(err){
+            throw err;
+        }
+    }
+
+    public async findUserAndHealthInfoByUserId(userId: string, manager: EntityManager){
+        try{
+            return await manager.createQueryBuilder(User, "user").leftJoinAndSelect("user.healthInfo", "healthInfo").where("user.user_id = :userId", {userId}).getOne();
+        }catch(err){
+            throw err;
+        }
+    }
+
+    public async findUserByUserId(userId: string, manager: EntityManager): Promise<User>{
+        try{
+            return await manager.createQueryBuilder(User, "user").select().where("user_id = :userId",{userId}).getOne();
+        }catch(err){
+            throw err;
+        }
+    }
+
+    // Update
+    public async updateUserByUserId(userId: string, user: User, manager: EntityManager){
+        try{
+            console.log("업데이트 될것", user);
+            return await manager.createQueryBuilder(User, "user").update(User).set(user).where("user_id = :userId",{userId}).execute();
+        }catch(err){
+            throw err;
+        }
+    }
+
+    public async updateHealthInfoIdByUserId(userId: string, healthInfo: HealthInfo, manager: EntityManager){
+        try{
+            return await manager.createQueryBuilder(User, "user").leftJoin('user.healthInfo', "healthInfo").update(User).set({healthInfo}).where("user_id = :userId",{userId}).execute();
+        }catch(err){
+            throw err;
+        }
+    }
+
+    // soft Delete
+    public async softDeleteUserByUserId(userId: string, manager: EntityManager){       
+        try{
+            console.log(userId);
+            return await manager.createQueryBuilder(User, "user").softDelete().where("user_id = :userId",{userId}).execute();
+        }catch(err){
+            throw err;
+        }
+    }
+
 }
