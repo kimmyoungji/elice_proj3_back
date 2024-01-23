67feb329e9883a9f13072f79db60d318b6c67bf1
hyunjinb <hyunjinb394@gmail.com>
2024-01-23 14:29:56 +0900

    Fix : 수정된 cumulative mvp body 형식대로 코드 수정

--- src/components/pages/record/Record.tsx
+++ src/components/pages/record/Record.tsx
@@ -9,10 +9,11 @@ const mealLogo = '/images/9gram_logo_box.png';
 // ✅ path = '/record/:selectedDate' url의 date를 가져와서 요청
 // ✅ date 값이 없으면 오늘 날짜로 보여주기
 // ✅ api 요청으로 선택한 날짜의 아점저간 total 칼로리만 가져옴
+// ✅ /cumulative-record/meal?date=2024-01-03
 // ✅ 특정 meal 선택시 MealPage로 이동
 
 const Record = () => {
-  const [foodData, setFoodData] = useState(recordData.food);
+  const [foodData, setFoodData] = useState(recordData.dateArr);
   const params = useParams();
   const selectedDate = params.selectedDate;
   // 가져온 날짜로 api 통신 data 가져오기
@@ -39,51 +40,51 @@ const Record = () => {
     <div>
       <div className={style.meal_container}>
         <div className={style.meal_header}> {headerDate} </div>
-        {Object.entries(foodData).map(([key, mealData], index) => (
+        {foodData.map((mealData, index) => (
           <div
-            onClick={() => handleMealClick(Number(key))}
+            onClick={() => handleMealClick(mealData[0])}
             key={index}
             className={style.meal_content}
           >
             <div className={style.meal_info}>
-              {mealData.mealCalories && mealData.img !== null ? (
+              {mealData[1] || mealData[2] ? (
                 <>
                   <img
                     className={style.meal_contentBackground}
-                    src={mealData.img || mealLogo}
+                    src={mealData[2] || mealLogo}
                     alt='하루 식단 이미지'
                   />
                   <div className={style.meal_time}>
-                    {mapSelectMealToMsg[Number(key)]}
+                    {mapSelectMealToMsg[mealData[0]]}
                   </div>
                   <div className={style.meal_calories}>
                     {' '}
-                    {mealData.mealCalories ?? 0} kcal{' '}
+                    {mealData[1] ?? 0} kcal{' '}
                   </div>
                 </>
               ) : (
                 <>
                   <div className={style.defaultBackground}></div>
                   <div className={style.default_time}>
-                    {mapSelectMealToMsg[Number(key)]}
+                    {mapSelectMealToMsg[mealData[0]]}
                   </div>
                   <div className={style.default_calories}> 0 kcal </div>
                 </>
               )}
             </div>
-            {mealData.mealCalories && mealData.img !== null ? (
-              <img
-                className={style.meal_button}
-                src='/icons/meal_delete.png'
-                alt='식단 삭제 버튼'
-              />
-            ) : (
-              <img
-                className={style.meal_button}
-                src='/icons/meal_plus_button.png'
-                alt='식단 추가 버튼'
-              />
-            )}
+            <img
+              className={style.meal_button}
+              src={
+                !mealData[1] && !mealData[2]
+                  ? '/icons/meal_plus_button.png'
+                  : '/icons/meal_delete.png'
+              }
+              alt={
+                !mealData[1] && !mealData[2]
+                  ? '하루 식단 추가 버튼'
+                  : '하루 식단 삭제 버튼'
+              }
+            />
           </div>
         ))}
       </div>
