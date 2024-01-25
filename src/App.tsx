6ce10862c7dadab9a4542f5d92214c1ce0f56e52
hyunjinb <hyunjinb394@gmail.com>
2024-01-25 13:38:36 +0900

    Feat: App.tsx, withAuth HOC 코드 추가해둠

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,58 @@ import {
   getKeyFromUrl,
   getNavProps,
 } from '@utils/getNavProps';
+
 import { lazy, Suspense } from 'react';
 import { useLocation, Navigate, Route, Routes } from 'react-router-dom';
 import { TopNavKeyType } from 'typings/propTypes';
 import Loading from '@components/UI/Loading';
+// import withAuth from '@components/hoc/withAuth';
 
-const Home = lazy(() => import('@components/pages/home/Home'));
 const Login = lazy(() => import('@components/pages/login/Login'));
 const Auth = lazy(() => import('@components/pages/join/Auth'));
-const Onboarding = lazy(() => import('@components/pages/join/Onboarding'));
 const Join = lazy(() => import('@components/pages/join/Join'));
+
+//hoc 적용 필요시 주석 해제 후 사용
+// const Onboarding = withAuth(
+//   lazy(() => import('@components/pages/join/Onboarding'))
+// );
+// const Home = withAuth(lazy(() => import('@components/pages/home/Home')));
+// const MyPage = withAuth(lazy(() => import('@components/pages/my-page/MyPage')));
+// const MyPageEdit = withAuth(
+//   lazy(() => import('@components/pages/my-page/MyPageEdit'))
+// );
+// const AddPhoto = withAuth(
+//   lazy(() => import('@components/pages/add-photo/AddPhoto'))
+// );
+// const AddPhotoSearch = withAuth(
+//   lazy(() => import('@components/pages/add-photo/AddPhotoSearch'))
+// );
+// const AiAnalyze = withAuth(
+//   lazy(() => import('@components/pages/ai-analyze/AiAnalyze'))
+// );
+// const AiDrawer = withAuth(
+//   lazy(() => import('@components/pages/ai-analyze/AiDrawer'))
+// );
+// const AiDrawerDetail = withAuth(
+//   lazy(() => import('@components/pages/ai-analyze/AiDrawerDetail'))
+// );
+// // const Record = withAuth(lazy(() => import('@components/pages/record/Record')));
+// // const RecordEdit = withAuth(
+// //   lazy(() => import('@components/pages/record/RecordEdit'))
+// // );
+// const MealPage = withAuth(
+//   lazy(() => import('@components/pages/record/MealPage'))
+// );
+// const Calender = withAuth(
+//   lazy(() => import('@components/pages/calendar/Calendar'))
+// );
+
+const Onboarding = lazy(() => import('@components/pages/join/Onboarding'));
+const Home = lazy(() => import('@components/pages/home/Home'));
+
 const MyPage = lazy(() => import('@components/pages/my-page/MyPage'));
 const MyPageEdit = lazy(() => import('@components/pages/my-page/MyPageEdit'));
+
 const AddPhoto = lazy(() => import('@components/pages/add-photo/AddPhoto'));
 const AddPhotoSearch = lazy(
   () => import('@components/pages/add-photo/AddPhotoSearch')
@@ -31,7 +71,6 @@ const Record = lazy(() => import('@components/pages/record/Record'));
 const RecordEdit = lazy(() => import('@components/pages/record/RecordEdit'));
 const MealPage = lazy(() => import('@components/pages/record/MealPage'));
 const Calender = lazy(() => import('@components/pages/calendar/Calendar'));
-
 const preventNavArr = ['login', 'join', 'auth', 'onboardingstep'];
 const preventTopNavArr = ['auth', 'sharestep'];
 
@@ -51,12 +90,13 @@ function App() {
         <main className='main'>
           <Suspense fallback={<Loading />}>
             <Routes>
+              <Route path='*' element={<Navigate to='/' />} />
               <Route path='/' element={<Navigate to='/home' />} />
               <Route path='/login' element={<Login />} />
               <Route path='/auth' element={<Auth />} />
               <Route path='/onboarding/:step' element={<Onboarding />} />
               <Route path='/join' element={<Join />} />
-              {/* <Route path='/join/onboarding' element={<JoinOnboard />} /> */}
+              {/* <Route path='/join/onboarding' element={<JoinOnboard />} />  */}
               <Route path='/home' element={<Home />} />
               <Route path='/my-page' element={<MyPage />} />
               <Route path='/my-page/edit' element={<MyPageEdit />} />
