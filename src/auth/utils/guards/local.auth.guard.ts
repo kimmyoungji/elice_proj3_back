8894aee44c2c2db5141c8ce685fb2d3b1d93e468
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-02-01 16:23:23 +0900

    Fix: 로그인 오류메세지 수정으로 인한 구글 로그인 오류 해결.

--- src/auth/utils/guards/local.auth.guard.ts
+++ src/auth/utils/guards/local.auth.guard.ts
@@ -5,10 +5,10 @@ import { LocalLoginDto } from "src/auth/dto/localLoginDto";
 export class LocalAuthGuard extends AuthGuard('local') {
   async canActivate(context: ExecutionContext) {
     try{
-      const activate = (await super.canActivate(context)) as boolean; // 로그인 성공시 true, 실패시 false
       const request = context.switchToHttp().getRequest();
       const localLoginDto: LocalLoginDto = request.body;
       if(!localLoginDto.email || !localLoginDto.password) throw new HttpException("등록되지 않은 이메일 이거나, 유효하지 않은 비밀번호입니다.", HttpStatus.UNAUTHORIZED);
+      const activate = (await super.canActivate(context)) as boolean; // 로그인 성공시 true, 실패시 false
       await super.logIn(request); // 세션에 사용자 정보를 저장
       return activate;
     }catch(err){
